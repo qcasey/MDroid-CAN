74363a2a29393127d42dbd8c3d2628474a9fab84
MrDoctorKovacic <qcasey1@gmail.com>
2019-06-20 17:33:00 -0700

    Allowing  for shared config file

--- mdroid-can.py
+++ mdroid-can.py
@@ -3,13 +3,14 @@ from canard.hw import cantact
 import handlers
 import requests
 import logging
+import argparse
+import os
+import json
 
 # BE SURE TO CHANGE THESE
 # Logging address more than likely should be commented out
 LOGGING_ADDRESS = "http://localhost:5353/session/" # This is where we'll log the data with a RESTful post
-dev = cantact.CantactDev("/dev/ttyACM0") # Connect to CANable that enumerated as ttyACM0
-dev.set_bitrate(500000) # Set the bitrate to a 500kbps
-dev.start() # Go on the bus
+DEVICE = "/dev/ttyACM0"
 
 # Start logging
 logging.basicConfig(level=logging.DEBUG)
@@ -30,6 +31,39 @@ handlers = {
 	504: handlers.parseBrakePressure
 }
 
+def parseConfig():
+	global LOGGING_ADDRESS, DEVICE
+
+	# parse program arguments
+	parser = argparse.ArgumentParser(description='Read from BMW E46 CAN Bus, forward to REST API.')
+	parser.add_argument('--settings', action='store', required=True, help='Config file to load Device and API settings.')
+	args  = parser.parse_args()
+
+	# Overwrite defaults if settings file is provided
+	if args.settings:
+		if os.path.isfile(args.settings): 
+			try:
+				with open(args.settings) as json_file:
+					data = json.load(json_file)
+					if "CONFIG" in data:
+						# Setup MDroid API
+						if "MDROID_HOST" in data["CONFIG"]:
+							LOGGING_ADDRESS = data["CONFIG"]["MDROID_HOST"]
+						else:
+							logging.debug("MDROID_HOST not found in config file, not using MDroid API.")
+
+					# Setup device
+					if "CAN_DEVICE" in data["CONFIG"]:
+						DEVICE = data["CONFIG"]["CAN_DEVICE"]
+					else: 
+						logging.debug("CAN_DEVICE not found in config file, using defaults.")
+
+			except IOError as e:
+				logging.error("Failed to open settings file:"+args.settings)
+				logging.error(e)
+		else:
+			logging.error("Could not load settings from file"+str(args.settings))
+
 # Log the decoded values to MDroid Core
 def logFrame(decodedValues):
 	for key,value in decodedValues.iteritems():
@@ -49,17 +83,19 @@ def getFrame():
 	return [frame.id, frame.data]
 
 if __name__ == "__main__":
-	count = 0
+	parseConfig() # overwrite defaults if necessary
+	dev = cantact.CantactDev(DEVICE) # Connect to CANable that enumerated as ttyACM0
+	dev.set_bitrate(500000) # Set the bitrate to a 500kbps
+	dev.start() # Go on the bus
 
 	while True:
-		count += 1
 		id, data = getFrame()
 		logging.info(str(id)+" ("+str(hex(id))+"): "+str(data))
 
 		if id in handlers:
 			decodedValues = handlers[id](data)
 			logging.info(decodedValues)
-			if LOGGING_ADDRESS: 
+			if LOGGING_ADDRESS and decodedValues: 
 				logFrame(decodedValues)
 		else:
 			logging.debug("Unknown ID "+str(id)+" ("+str(hex(id))+")")
